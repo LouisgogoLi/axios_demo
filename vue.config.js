dc77b46e6a6a0c58d95cb4b26f143c53852f6511
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-03-18 11:22:00 +0800

    更新element echart

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
+const { defineConfig } = require("@vue/cli-service");
 const AutoImport = require("unplugin-auto-import/webpack");
 const Components = require("unplugin-vue-components/webpack");
 const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
 
-module.exports = {
+module.exports = defineConfig({
   transpileDependencies: true,
   pluginOptions: {
     i18n: {
@@ -15,12 +16,26 @@ module.exports = {
       fullInstall: true,
     },
   },
-  configureWebpack: {
-    devtool: "source-map",
-    plugins: [
-      AutoImport({ resolvers: [ElementPlusResolver()] }),
-      Components({ resolvers: [ElementPlusResolver()] }),
-    ],
+  css: {
+    extract: {
+      ignoreOrder: true,
+    },
+  },
+  configureWebpack: (config) => {
+    const plugins = [];
+    plugins.push(
+      AutoImport({ resolvers: [ElementPlusResolver({ importStyle: false })] })
+    );
+    plugins.push(Components({ resolvers: [ElementPlusResolver()] }));
+    config.plugins = [...config.plugins, ...plugins];
+
+    if (process.env.npm_lifecycle_event === "build:debug") {
+      config.devtool = "source-map";
+    } else if (process.env.NODE_ENV === "production") {
+      config.devtool = false;
+    } else {
+      config.devtool = "source-map";
+    }
   },
   publicPath: process.env.NODE_ENV === "production" ? "/axios_demo/" : "/",
-};
+});
