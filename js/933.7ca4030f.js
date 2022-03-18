d65c5cc972e6dcc1230451a06fe4d63e5a725bbc
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-03-18 11:23:42 +0800

    update 2022-03-18 11:23:42

---
+++ js/933.7ca4030f.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkapp"]=self["webpackChunkapp"]||[]).push([[933],{3933:function(n,a,u){u.r(a),u.d(a,{default:function(){return f}});var t=u(3396);const e={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(n,a){return(0,t.wg)(),(0,t.iD)("div",e,s)}var i=u(89);const p={},o=(0,i.Z)(p,[["render",c]]);var f=o}}]);
